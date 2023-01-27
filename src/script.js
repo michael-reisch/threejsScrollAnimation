d7965ca5281dcfb5ba429afe6439b0723aad0e88
Michael Reisch <mreisch88@gmail.com>
2023-01-27 17:58:26 +1300

    adding particles for depth experience

--- src/script.js
+++ src/script.js
@@ -60,6 +60,37 @@ mesh3.position.x = 2
 scene.add(mesh1, mesh2, mesh3)
 
 /**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = Math.random() - .5
+  positions[i * 3 + 1] = Math.random()
+  positions[i * 3 + 2] = Math.random() - .5
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+)
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+
+scene.add(particles)
+
+/**
  * Lights
  */
 
@@ -155,8 +186,8 @@ const tick = () => {
   // Animate Camera
   camera.position.y = (-scrollY / sizes.height) * 4
 
-  const parallaxX = cursor.x * .5
-  const parallaxY = -cursor.y * .5
+  const parallaxX = cursor.x * 0.5
+  const parallaxY = -cursor.y * 0.5
   cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
   cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
